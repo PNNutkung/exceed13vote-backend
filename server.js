9bf7fd310c0bf3f0729eba6cebbca490441c57ee
PNNutkung <pipatpol.t@ku.th>
2016-07-09 09:10:22 +0700

    Implement server.js

--- server.js
+++ server.js
@@ -8,3 +8,20 @@ var config = require('./config/database');
 var User = require('./app/models/user');
 var port = process.env.PORT || 8080;
 var jwt = require('jwt-simple');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+// Show log on console
+app.use(morgan('dev'))
+
+app.use(passport.initialize());
+
+app.get('/', (req, res) => {
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+
+app.listen(port);
+console.log('There will be dragons: http://localhost:' + port);
