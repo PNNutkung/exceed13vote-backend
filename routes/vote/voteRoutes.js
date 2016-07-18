8fbc2f3991df5052466dce460ed1001d51244679
PNNutkung <pipatpol.t@ku.th>
2016-07-18 09:49:48 +0700

    Add categoryCheck before all vote routes

--- routes/vote/voteRoutes.js
+++ routes/vote/voteRoutes.js
@@ -5,7 +5,7 @@ var config = require('./../../config/database');
 var jwt = require('jwt-simple');
 
 module.exports = module.exports = (apiRoutes, mongoose, isAuthenticated, decodeUsername) => {
-    apiRoutes.post('/vote/check_voted', (req, res) => {
+    apiRoutes.post('/vote/check_voted', isAuthenticated, categoryCheck, (req, res) => {
         var tokenUsername = decodeUsername(req.headers);
         User.findOne({
             username: tokenUsername
@@ -34,7 +34,7 @@ module.exports = module.exports = (apiRoutes, mongoose, isAuthenticated, decodeU
         });
     });
 
-    apiRoutes.post('/vote/average', (req, res) => {
+    apiRoutes.post('/vote/average', categoryCheck, (req, res) => {
         Vote.find({
                 vote_category: req.body.category
             })
@@ -52,22 +52,22 @@ module.exports = module.exports = (apiRoutes, mongoose, isAuthenticated, decodeU
                     total += vote.score;
                 });
                 Project.find({
-                    _id: req.body.project_id
-                })
-                .populate('group')
-                .exec((error, project) => {
-                    return res.json({
-                        status: 200,
-                        success: true,
-                        project: project[0].name,
-                        group: project[0].group.group_name,
-                        average: votes.length === 0 ? 0 : total / votes.length
-                    });
-                })
+                        _id: req.body.project_id
+                    })
+                    .populate('group')
+                    .exec((error, project) => {
+                        return res.json({
+                            status: 200,
+                            success: true,
+                            project: project[0].name,
+                            group: project[0].group.group_name,
+                            average: votes.length === 0 ? 0 : total / votes.length
+                        });
+                    })
             });
     });
 
-    apiRoutes.post('/vote', isAuthenticated, (req, res) => {
+    apiRoutes.post('/vote', isAuthenticated, categoryCheck, (req, res) => {
         var tokenUsername = decodeUsername(req.headers);
         User.findOne({
             username: tokenUsername
@@ -104,3 +104,22 @@ module.exports = module.exports = (apiRoutes, mongoose, isAuthenticated, decodeU
         });
     });
 }
+
+var categoryCheck = function(req, res, next) {
+    switch (req.body.category) {
+        case 'best_of_hardware':
+            return next();
+        case 'best_of_software':
+            return next();
+        case 'popular':
+            return next();
+        case 'top_rated':
+            return next();
+        default:
+            return res.json({
+                status: 201,
+                success: false,
+                message: 'Wrong category type!'
+            });
+    }
+};
