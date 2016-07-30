a494d1a744dc920747458a0101c1e932b8a662b5
taweesoft <taweesoft>
2016-07-29 23:28:36 -0400

    Change headers

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, project_id, Authorization, o-Requested-With");
+  next();
+});
 // Show log on console
 app.use(morgan('dev'))
 
@@ -19,7 +24,7 @@ app.get('/', (req, res) => {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
-app.listen(port);
+app.listen(port,"128.199.135.164");
 console.log('eXceed13vote online @ http://localhost:' + port);
 
 require('./routes/routesManager')(app, passport, express);
