d49b3ddeb78c9b2fee271da7ee13b00e571005ed
PNNutkung <pipatpol.t@ku.th>
2016-07-30 10:51:54 +0700

    Delete unuse class

--- routes/timeRoutes/timeRoutes.js
+++
@@ -1,9 +0,0 @@
-module.exports = (apiRoutes, errorHandle) => {
-    apiRoutes.get('/time', errorHandle, (req, res) => {
-        return res.json({
-            message: 'eXceed vote will be closed in',
-            round: 'document',
-            remain_time: new Date("2016-07-29T12:00:00")
-        });
-    });
-}
