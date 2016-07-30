5187a93d7d3787e038d22e8a891d1a6a062f3bff
PNNutkung <pipatpol.t@ku.th>
2016-07-30 18:10:33 +0700

    remove unique

--- app/models/vote.js
+++ app/models/vote.js
@@ -7,16 +7,13 @@ var VoteSchema = new Schema({
     vote_user: {
         type: Schema.ObjectId,
         ref: 'User',
-        unique: true
     },
     vote_category: {
         type: String,
-        unique: true
     },
     project: {
         type: Schema.ObjectId,
         ref: 'Project',
-        unique: true
     },
     score: {
         type: Number
